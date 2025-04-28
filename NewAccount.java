529fcee3aee12e0a8e0897c0c8ce7ea31e6b0fda
KOTESWARARAO611 <kotikandukuri9515@gmail.com>
2025-04-28 11:18:26 +0530

    update-2

--- NewAccount.java
+++ NewAccount.java
@@ -6,38 +6,44 @@ public class NewAccount {
 	private Long AcNumber;
 	private int pin;
 	private double initialBalance;
-	
-	
-	public NewAccount(String name,String location,Long acnumber,int pin,double balval) {
-		this.username=name;
-		this.location=location;
-		this.AcNumber=acnumber;
-		this.pin=pin;
-		this.initialBalance=balval;	
+
+	public NewAccount(String name, String location, Long acnumber, int pin, double balval) {
+		this.username = name;
+		this.location = location;
+		this.AcNumber = acnumber;
+		this.pin = pin;
+		this.initialBalance = balval;
 	}
-	public NewAccount(){
-		
+
+	public NewAccount() {
+
 	}
+
 	public String getusername() {
 		return username;
 	}
+
 	public String getlocation() {
 		return location;
 	}
+
 	public Long getAcNumber() {
 		return AcNumber;
 	}
+
 	public int getpin() {
 		return pin;
 	}
+
 	public double getinitialBalance() {
 		return initialBalance;
 	}
+
 	public void setbalance(double balance) {
-		this.initialBalance=balance;
+		this.initialBalance = balance;
 	}
+
 	public static void main(String[] args) {
-		
 
 	}
 
